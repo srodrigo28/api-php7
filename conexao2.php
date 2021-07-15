514944fd5370606c2cc157eb68a9ac95a6b8ebd0
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2021-07-15 13:40:24 -0300

    3 commit

---
+++ conexao2.php
@@ -0,0 +1,14 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "celke";
+$porta = "3306";
+
+//$conn = new PDO("mysql:host=$host;dbname=" . $dbname, $user, $pass);
+try{
+    $conn = new PDO("mysql:host=$host;port=$porta;dbname=" . $dbname, $user, $pass);
+}catch(PDOException $e){
+    echo $e;
+}
